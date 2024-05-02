a226be16191969a22b4142f614b03f853f7bcb3e
Daria Dmitrienko <ishmizh@gmail.com>
2024-05-02 20:34:05 +0400

    1 коммит

---
+++ index.js
@@ -0,0 +1,61 @@
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+}
+
+function initCopyButtons() {
+  const copyButtons = document.querySelectorAll(".js-copy-button");
+  for (let btn of copyButtons) {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetButton = e.target;
+      const defaultText = targetButton.textContent;
+      const valueToCopy = targetButton.dataset.value;
+      try {
+        copyTextToClipboard(valueToCopy);
+        targetButton.textContent = "Copied";
+        setTimeout(() => {
+          targetButton.textContent = defaultText;
+        }, 2000);
+      } catch (e) {
+        console.log(e, "error");
+        targetButton.textContent = "Failed to copy";
+      }
+    });
+  }
+}
+
+initCopyButtons();
